766ad2151bb53164083d3ad728e59dc66b54382e
juan <juanalexislozano011@gmail.com>
2019-11-19 09:56:32 +0800

    Content

---
+++ abi_contents/Abi_ContentFormsViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Abi_ContentFormsViewCell.swift
+//  abi_contents
+//
+//  Created by Bill Gates on 11/19/19.
+//  Copyright © 2019 juan. All rights reserved.
+//
+
+import UIKit
+
+class Abi_ContentFormsViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
